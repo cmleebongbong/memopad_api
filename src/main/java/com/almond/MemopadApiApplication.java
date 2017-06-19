74acde033ed8a74071fc7314a625d0660dc78960
cmlee <dlcndaks12@naver.com>
2017-06-19 13:13:44 +0900

    init

---
+++ src/main/java/com/almond/MemopadApiApplication.java
@@ -0,0 +1,12 @@
+package com.almond;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemopadApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemopadApiApplication.class, args);
+	}
+}
