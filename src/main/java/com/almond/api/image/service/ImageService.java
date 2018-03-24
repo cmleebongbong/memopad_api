6deb99f6be0c9653c57a634fe0ddd459fa603c88
dlcndaks12 <dlcndaks12@naver.com>
2018-03-24 15:41:46 +0900

    url to byte[] 파싱 하지않고 그대로 읽어 보냄

--- src/main/java/com/almond/api/image/service/ImageService.java
+++ src/main/java/com/almond/api/image/service/ImageService.java
@@ -2,6 +2,7 @@ package com.almond.api.image.service;
 
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
 import java.net.URL;
 
 import javax.imageio.ImageIO;
@@ -12,18 +13,46 @@ import org.springframework.stereotype.Service;
 public class ImageService {
 	
 	/**
-	 * 이미지 Url to Byte Array
+	 * 이미지 Url 그대로 to Byte Array
+	 * 
+	 * @param urlStr
+	 * @return byte[]
+	 * @throws Exception
+	 */
+	public byte[] imageToByte(String urlStr) throws Exception {
+    	URL url = new URL(urlStr);
+        ByteArrayOutputStream output = new ByteArrayOutputStream();
+        
+        try (InputStream inputStream = url.openStream()) {
+            int n = 0;
+            byte [] buffer = new byte[ 1024 ];
+            while (-1 != (n = inputStream.read(buffer))) {
+                output.write(buffer, 0, n);
+            }
+        }
+    
+        return output.toByteArray();
+	}
+	
+	
+	/**
+	 * 이미지 Url 파싱후 to Byte Array
 	 * 
 	 * @param url
 	 * @return byte[]
 	 * @throws Exception
 	 */
-	public byte[] imageToByte(String url) throws Exception {
+	public byte[] imageToParseredByte(String url) throws Exception {
     	URL imgUrl = new URL(url);
     	BufferedImage bufferedimage = ImageIO.read(imgUrl);
+    	
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        ImageIO.write(bufferedimage, "png", baos);
+        
+        ImageIO.write(bufferedimage, "jpg", baos);
+      
+        baos.flush();
         byte[] imageData = baos.toByteArray();
+        baos.close();
 		
 		return imageData;
 	}
